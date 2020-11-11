1d7a838016c5db4a593d43f819884f73b960508a
Mateo Gonzales Navarrete <38146507+mgonnav@users.noreply.github.com>
2020-11-10 23:00:12 -0500

    :zap: Hide Turnos & Actividades from admin page. (#14)
    Edit actividades from related event page
    Edit turnos from related Actividad page

--- bugevents/configuracion/admin.py
+++ bugevents/configuracion/admin.py
@@ -3,26 +3,49 @@ from django.contrib import admin
 from .models import Ambiente, Evento, Actividad, Ponente, Turno, Material
 from .forms import AmbienteForm, EventoForm, TurnoForm, MaterialForm
 
+
 class AmbienteAdmin(admin.ModelAdmin):
     form = AmbienteForm
 admin.site.register(Ambiente, AmbienteAdmin)
 
-admin.site.register(Actividad)
+
 class ActividadInline(admin.TabularInline):
     model = Actividad
+    show_change_link = True
+
 
 class EventoAdmin(admin.ModelAdmin):
-    inlines = [ActividadInline,]
     form = EventoForm
+    inlines = [ActividadInline]
 admin.site.register(Evento, EventoAdmin)
 
 
 admin.site.register(Ponente)
 
+
 class TurnoAdmin(admin.ModelAdmin):
     form = TurnoForm
+
+    def get_model_perms(self, request):
+        return {}
 admin.site.register(Turno, TurnoAdmin)
+
 
 class MaterialAdmin(admin.ModelAdmin):
     form = MaterialForm
 admin.site.register(Material,MaterialAdmin)
+
+
+class TurnoInline(admin.TabularInline):
+    model = Turno
+
+
+class ActividadAdmin(admin.ModelAdmin):
+    model = Actividad
+    inlines = [TurnoInline]
+
+    def get_model_perms(self, request):
+        return {}
+
+
+admin.site.register(Actividad, ActividadAdmin)
